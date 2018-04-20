84216159266916d42ca0b660a935e26ddd128595
Ben Worthington <worthingtonb@bentonvillek12.org>
2018-04-20 14:00:43 -0500

    Continue working on the DetailActivity.java

--- app/src/main/java/com/worthingtonb/bawdev/listapp/DetailActivity.java
+++ app/src/main/java/com/worthingtonb/bawdev/listapp/DetailActivity.java
@@ -1,7 +1,8 @@
-package com.worthingtonb.bawdev.listapp;
+    package com.worthingtonb.bawdev.listapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent in = getIntent();
+        int index = in.getIntExtra("com.worthingtonb.bawdev.ITEM_INDEX", -1);
+
     }
 }
